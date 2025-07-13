b20450c835a940318ca12618789f2e94a32e60cf
ashutosh229 <akumarjha875@gmail.com>
2025-07-13 22:05:28 +0530

    pushed

---
+++ upper_bound.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lower_bound(vector<int> &nums, int low, int high, int target)
+{
+    int ans = -1;
+    int mid;
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else if (nums[mid] > target)
+        {
+            high = mid - 1;
+            ans = mid;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int upper_bound(vector<int> &nums, int low, int high, int target)
+{
+    int ans = -1;
+    int mid;
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            low = mid + 1;
+        }
+        else if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+    int target;
+    cin >> target;
+    int low = 0;
+    int high = n - 1;
+    int ans = lower_bound(nums, low, high, target);
+    cout << ans << endl;
+    int ans2 = upper_bound(nums, low, high, target);
+    cout << ans2 << endl;
+    return 0;
+}
