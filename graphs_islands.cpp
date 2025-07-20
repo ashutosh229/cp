e9f2112a262312beedf05d1148d27c7f330e2a36
ashutosh229 <akumarjha875@gmail.com>
2025-07-21 01:36:11 +0530

    pushed

--- graphs_islands.cpp
+++ graphs_islands.cpp
@@ -6,20 +6,22 @@ void dfs(vector<vector<char>> &grid, int m, int n, pair<int, int> source)
     int i = source.first;
     int j = source.second;
     if (i < 0 || i >= m || j < 0 || j >= n)
-    {
+    { // using cond2 to check for edge and if true then go ahead
         return;
     }
     if (grid[i][j] == '0')
-    {
+    { // using cond3 to check for edge and if true then go ahead
         return;
     }
 
-    visited[i][j] = true;
+    visited[i][j] = true; // taking actions after entering the node
 
     dfs(grid, m, n, {i - 1, j});
     dfs(grid, m, n, {i, j + 1});
     dfs(grid, m, n, {i + 1, j});
     dfs(grid, m, n, {i, j - 1});
+
+    // taking actions before exiting the node
 }
 
 int numIslands(vector<vector<char>> &grid)
@@ -34,10 +36,10 @@ int numIslands(vector<vector<char>> &grid)
         {
             pair<int, int> source = {i, j};
             if (grid[i][j)!='0')
-                {
+                { // using cond1 to check for node and if node then go ahead
                     if (!visited[i][j])
                     {
-                        dfs();
+                        dfs(grid, m, n, source);
                         counter++;
                     }
                 }
